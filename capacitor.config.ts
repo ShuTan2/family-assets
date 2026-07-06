dcf665e90129a62735c615264e153cb1a31dcfee
ShuTan2 <shutan2@users.noreply.github.com>
2026-07-06 17:54:16 +0800

    Updates

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.family.assets',
-  appName: '家庭资产记账',
-  webDir: 'dist'
-};
-
-export default config;
